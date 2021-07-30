132ed52d2a4d17c64082c31b4c2b80327851e798
ac-johnson <acjohnson149@gmail.com>
2021-07-30 15:46:15 -0800

    call more runs

--- doruns.py
+++ doruns.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+
+Run pism commands. To run this script put in:
+    
+python doruns.py runstart runstop scenario1 scenario2 ...
+
+ex: python doruns.py 50 75 0C 1C 2C 4C 6C
+
 Created on Fri Mar 30 11:49:07 2018
 
 @author: andrewjohnson
@@ -16,7 +23,11 @@ runlist_stop = int(sys.argv[2])
 runlist = range(runlist_start,runlist_stop)
 
 #templist=['0C','2C','4C','6C']
-templist = ['0C','2C','4C']
+#templist = ['0C','2C','4C']
+templist = []
+
+for t in sys.argv[3:]:
+    templist.append(t)
 
 rundir = '/import/c1/ICESHEET/ICESHEET/uaf-antarctica/filron/filron_runs/'
 
