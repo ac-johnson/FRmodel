50cfa80d22af7babcd695c722955293ab60b4fce
ac-johnson <acjohnson149@gmail.com>
2021-08-02 12:17:49 -0800

    call more runs

---
+++ remove_backups.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  2 12:14:22 2021
+
+@author: andrew
+"""
+
+import os
+import sys
+#import glob
+
+#runtemp = sys.argv[1]
+
+runlist_start = int(sys.argv[1])
+runlist_stop = int(sys.argv[2])
+#runtemp = sys.argv[3]
+
+#
+runlist = range(runlist_start,runlist_stop)
+#
+##templist=['0C','2C','4C','6C']
+##templist = ['0C','2C','4C']
+templist = []
+#
+for t in sys.argv[3:]:
+    templist.append(t)
+
+rundir = '/import/c1/ICESHEET/ICESHEET/uaf-antarctica/filron/filron_runs/'
+#rundir = rundir+'picoruns/ocean_'+runtemp+'/output/'
+
+os.chdir(rundir)
+#print(os.system('pwd'))
+
+
+
+#flist = glob.glob(rundir+'picoruns/ocean_'+runtemp+'/output/*.nc')
+
+
+
+for i in runlist:
+    for T in templist:
+        opdir = rundir+'picoruns/ocean_'+T+'/output/'
+#        os.system('sbatch picoruns/ocean_%s/runcmds/pico_run%s.slurm'%(T,i))
+        os.system('rm %sresult_medium_%i_backup.nc'%(opdir,i))
+        os.system('rm %sresult_medium_%i_backup.nc~'%(opdir,i))
+#        os.system('%sresult_%i.nc %sresult_thin_%i.nc'%(opdir,i,opdir,i))
